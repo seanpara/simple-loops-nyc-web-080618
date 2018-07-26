8084b67db7c3600669dc3d2f3d858360f0079408
Sean Logan Para <spara77@gmail.com>
2018-07-26 18:47:12 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,9 +12,10 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0 
   loop do 
-    
-  
-  
+    puts phrase 
+    counter += 1 
+    break if counter = number_of_times
+  end 
 end
 
 def times_iterator(number_of_times)
